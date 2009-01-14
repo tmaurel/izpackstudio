f17270a951a81f6880a6ea08ab3a35f64e70e838
unknown <alexis@.(none)>
2009-01-14 19:55:46 +0100

    added first unit test

---
+++ src/tests/IzPackStudioTest.groovy
@@ -0,0 +1,12 @@
+package tests
+
+import org.junit.Test
+import static org.junit.Assert.assertEquals
+
+class IzPackStudioTest {
+     @Test
+    void methode()
+     {
+         assertEquals 4,2+2
+     }
+}
